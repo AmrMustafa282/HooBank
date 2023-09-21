e4d616a32573a530d3fb9ae13d1554740d2f10c9
AmrMustafa282 <amrdarwish115@gamil.com>
2023-09-21 23:10:51 +0300

    first

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+
+
+const Button = ({styles}) => 
+   (
+    <button type="button" className={`py-4 px-6 bg-blue-gradient
+    font-poppins font-medium text-[18px] text-primary ouline-none
+    rounded-[10px] ${styles}`}>
+      Get Started
+    </button>
+  )
+
+
+export default Button
